f0559c11afff6068827a40d1700b09fa288ccae7
ehurafa <rafarfl2010@gmail.com>
2022-03-18 18:58:33 -0400

    criando a matriz de numeros

---
+++ numbers/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app">
+    <div v-for="(item, index) in 10" :key="index">
+      <NumberApp :num="(index) * 10" />
+    </div>
+  </div>
+</template>
+
+<script>
+import NumberApp from './components/NumberApp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NumberApp
+  },
+  data () {
+    return {
+      arr: [2, 4]
+    }
+  }
+}
+</script>
+
+<style>
+.app {
+  padding: 20px;
+  background: #222;
+}
+</style>
---
+++ numbers/src/components/NumberApp.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="blocks">
+    <div v-for="(item, index) in arr" :key="index" class="block">
+      <p>{{ (item + num) }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NumberApp',
+  data () {
+    return {
+      arr: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    }
+  },
+  props: {
+    num: Number
+  }
+}
+</script>
+
+<style scoped>
+.blocks {
+  display: flex;
+  justify-content: space-evenly;
+}
+.block {
+  border: 1px solid #000;
+  width: 100%;
+  height: 100px;
+  align-items: stretch;
+  text-align: center;
+}
+.block p {
+  color: #fff;
+}
+</style>
